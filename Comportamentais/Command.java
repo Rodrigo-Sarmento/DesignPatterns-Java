a600c5dea8acc90c16f0fdfba6295fc94b4bdb0d
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2024-09-03 01:32:41 -0300

    Create Command.java

---
+++ Comportamentais/Command.java
@@ -0,0 +1,48 @@
+// Command
+interface Command {
+    void execute();
+}
+
+// Concrete Command
+class ConcreteCommand implements Command {
+    private Receiver receiver;
+
+    public ConcreteCommand(Receiver receiver) {
+        this.receiver = receiver;
+    }
+
+    public void execute() {
+        receiver.action();
+    }
+}
+
+// Receiver
+class Receiver {
+    void action() {
+        System.out.println("Receiver action");
+    }
+}
+
+// Invoker
+class Invoker {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void invoke() {
+        command.execute();
+    }
+}
+
+// Client code
+public class CommandExample {
+    public static void main(String[] args) {
+        Receiver receiver = new Receiver();
+        Command command = new ConcreteCommand(receiver);
+        Invoker invoker = new Invoker();
+        invoker.setCommand(command);
+        invoker.invoke();
+    }
+}
